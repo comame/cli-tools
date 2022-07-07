6c1f1426fa530633c7343ef9c959ed17772baf0b
comame <dev@comame.xyz>
2022-07-08 00:43:49 +0900

    added runner

---
+++ runner/bin/index.js
@@ -0,0 +1,61 @@
+#! /usr/bin/node
+
+const { resolve } = require('node:path')
+const { spawn } = require('node:child_process')
+const { argv, exit, stdin, stdout, stderr } = require('node:process')
+const commands = require('../../commands.json')
+
+const subcommand = argv[2]
+
+switch (subcommand) {
+    case 'install':
+        install()
+        break
+    default:
+        const path = commands[subcommand]
+        if (!path) {
+            console.error('invalid subcommand')
+            exit(1)
+        }
+        run(path)
+}
+
+function run(path) {
+    const cmd = [ resolve(__dirname, '../../', path), ...argv.slice(3)]
+        .map(it => `"${it}"`).join(' ')
+    _exec(cmd)
+}
+
+async function install() {
+    const zipUrl = 'https://github.com/comame/cli-tools/archive/refs/heads/main.zip'
+    const tmpCwd = '/tmp/cli-tools'
+    const downloadFile = tmpCwd + '/c-runner-download.zip'
+    const extractedDir = 'cli-tools-main'
+
+    await _exec(`mkdir -p ${tmpCwd}`)
+    await _exec(`curl -sSLf -o ${downloadFile} ${zipUrl}`)
+    await _exec(`unzip -o ${downloadFile} 1>/dev/null`, { cwd: tmpCwd })
+    await _exec(`sudo rm -rf /usr/local/lib/cli-tools`)
+    await _exec(`sudo mv -f ${tmpCwd}/${extractedDir} /usr/local/lib/cli-tools`)
+    await _exec(`sudo rm -f /usr/local/bin/c`)
+    await _exec(`sudo ln -s /usr/local/lib/cli-tools/runner/bin/index.js /usr/local/bin/c`)
+
+    for (const command of Object.keys(commands)) {
+        const path = '/usr/local/lib/cli-tools/' + commands[command]
+        await _exec(`sudo chmod +x ${path}`)
+    }
+}
+
+async function _exec(command, opt = undefined) {
+    const proc = spawn('/bin/bash', [ '-c', command ], opt)
+
+    stdin.pipe(proc.stdin)
+    proc.stdout.pipe(stdout)
+    proc.stderr.pipe(stderr)
+
+    return new Promise((resolve) => {
+        proc.on('exit', () => {
+            resolve()
+        })
+    })
+}
