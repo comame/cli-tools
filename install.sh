1c465ba6bae21a044ebbf78d008ef68736525f0a
comame <dev@comame.xyz>
2023-04-17 12:40:14 +0900

    install.sh を直した

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 curl -sSLf https://raw.githubusercontent.com/comame/cli-tools/main/cli-tools/bin/index.js -o /tmp/cli-tools-installer.js
 node /tmp/cli-tools-installer.js install
-~/.local/bin/cli-tools install
+~/.local/bin/tools install
 echo "Run \`echo \"export PATH=\\\$PATH:\\\$HOME/.local/bin\" >> ~/.bashrc\` as needed."
